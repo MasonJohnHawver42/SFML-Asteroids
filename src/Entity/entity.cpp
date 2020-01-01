bf6dfab26445db5a17283cb20e2c37ae1f02eb77
Mason Hawver <33504840+MasonJohnHawver42@users.noreply.github.com>
2019-12-31 20:41:03 -0600

    Update entity.cpp

--- src/Entity/entity.cpp
+++ src/Entity/entity.cpp
@@ -18,7 +18,8 @@ protected:
   double maxTSpeed;
   double maxTAcc;
 
-  Shape * body;
+  Polygon * body;
+  double density;
 
   int id;
   static int nextId;
@@ -41,7 +42,8 @@ public:
     maxTSpeed = 10;
     maxTAcc = 1 / 10.0;
 
-    body = new Shape;
+    body = new Polygon();
+    density = 1;
 
     id = nextId;
     nextId++;
@@ -61,7 +63,9 @@ public:
   double getMaxTurningSpeed() { return maxTSpeed; }
   double getMaxTurningAcc() { return maxTAcc; }
 
-  Shape * getBody() { return body; }
+  Polygon * getBody() { return body; }
+  double getDensity() { return density; }
+  double getMass() { return ( density * body->getArea() ); }
 
   int getId() { return id; }
 
@@ -82,17 +86,78 @@ public:
   void setMaxTurningSpeed(double ms) { maxTSpeed = ms; }
   void setMaxTurningAcc(double ma) { maxTAcc = ma; }
 
-
-  void setBody(Shape * b) { body = b; }
+  void setBody(Polygon * b) { body = b; }
+  void setDensity(double d) { density = d; }
 
   // modifiers
 
   void turn(double radOffset) { dir->turn(radOffset); }
 
-  void addAcc(Vector<double> * a) { acc->add(* a); }
+  virtual void addAcc(Vector<double> * a) { acc->add(* a); }
   void addTurningAcc(double a) { turningAcc += a; }
 
   // other
+
+  virtual bool collide(Entity * other) {
+    double dis = pos->getDis(*other->getPos());
+    dis = dis - body->getSize() - other->getBody()->getSize();
+
+    if( dis > 0 ) { return 0; }
+
+    else {
+
+      Vector<double> * posDiff = Vector<double>::sub(*pos, *other->getPos());
+
+      double slope = posDiff->getY() / posDiff->getX();
+
+      double b1 = vel->getY() + ( vel->getX() / slope );
+      double b2 = other->getVel()->getY() + ( other->getVel()->getX() / slope );
+
+      Vector<double> * myBasisVectorX = new Vector<double>(0, 0);
+      myBasisVectorX->setX(b1 / (slope + ( 1 / slope ) ) );
+      myBasisVectorX->setY(slope * myBasisVectorX->getX());
+
+      Vector<double> * myBasisVectorY = Vector<double>::sub(*vel, *myBasisVectorX);
+
+      Vector<double> * otherBasisVectorX = new Vector<double>(0, 0);
+      otherBasisVectorX->setX(b2 / (slope + ( 1 / slope ) ) );
+      otherBasisVectorX->setY(slope * otherBasisVectorX->getX());
+
+      Vector<double> * otherBasisVectorY = Vector<double>::sub(*other->getVel(), *otherBasisVectorX);
+
+      double myVel = myBasisVectorX->getMag();
+      myVel *= ( myBasisVectorX->getX() > 0 ? 1 : -1 );
+
+      double otherVel = otherBasisVectorX->getMag();
+      otherVel *= ( otherBasisVectorX->getX() > 0 ? 1 : -1 );
+
+      double myMass = getMass();
+      double otherMass = other->getMass();
+
+      double myFinalVel = ( ( myVel * ( myMass - otherMass ) ) + ( 2 * otherMass * otherVel ) ) / ( myMass + otherMass );
+      double otherFinalVel = ( ( otherVel * ( otherMass - myMass ) ) + ( 2 * myMass * myVel ) ) / ( myMass + otherMass );
+
+      double x1 = sqrt( ( myFinalVel * myFinalVel ) / ( 1 + ( slope * slope ) ) ) * (myFinalVel > 0 ? 1 : -1);
+      double y1 = x1 * slope;
+
+      double x2 = sqrt( ( otherFinalVel * otherFinalVel ) / ( 1 + ( slope * slope ) ) ) * (otherFinalVel > 0 ? 1 : -1);
+      double y2 = x2 * slope;
+
+      Vector<double> * myNewVel = new Vector<double>(x1, y1);
+      myNewVel->add(*myBasisVectorY);
+
+      Vector<double> * otherNewVel = new Vector<double>(x2, y2);
+      otherNewVel->add(*otherBasisVectorY);
+
+      setVel(myNewVel);
+      other->setVel(otherNewVel);
+
+    }
+
+
+    return 1;
+  }
+
 
   virtual bool equal(Entity * other) { return ( id == other->getId() ); }
 
@@ -102,3 +167,4 @@ public:
 };
 
 int Entity::nextId = 0;
+
