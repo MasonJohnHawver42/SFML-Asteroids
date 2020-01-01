2c8b0aac4235dfaaa1653f35eefdcf1899c7afd9
Mason Hawver <33504840+MasonJohnHawver42@users.noreply.github.com>
2019-12-31 20:40:27 -0600

    Update asteroid.cpp

--- src/Entity/asteroid.cpp
+++ src/Entity/asteroid.cpp
@@ -1,6 +1,6 @@
-#include"ship.cpp"
+#include"worldEntity.cpp"
 
-class Asteroid : public Entity {
+class Asteroid : public WorldEntity {
 private:
   int vertCount;
 
@@ -8,17 +8,39 @@ private:
 
   void generateBody() {
 
+    Polygon * newBody = new Polygon();
+
+    double ang = 0;
+
+    for(int i = 0; i < vertCount; i++)
+    {
+      Vector<double> * vert = Vector<double>::radianToVector(ang);
+      vert->mult(size);
+      newBody->addVertex(vert);
+
+      ang += (M_PI * 2.0) / vertCount;
+    }
+
+    setBody(newBody); return;
   }
 
 public:
   Asteroid() : WorldEntity() {
     vertCount = 5;
     size = 1.0;
+
+    generateBody();
   }
 
   Asteroid(int vc, double s) : WorldEntity() {
-    vertCount = vd;
+    vertCount = vc;
     size = s;
+
+    generateBody();
+
+    maxSpeed = 100;
+
+    density = 1;
   }
 
   //getters
@@ -33,10 +55,27 @@ public:
 
   void setSize(double s) { size = s; }
 
+  void setWorld(World * w) { world = w; world->addAsteroid(this); }
+
   // other
 
   void implode() {}
 
+  virtual void update() {
+
+    applyVel();
+
+    warpPos(size, size);
+
+    updateDir();
+
+    vel->limitMag(maxSpeed);
+
+    clock->restart();
+
+  }
+
 
 
 };
+
